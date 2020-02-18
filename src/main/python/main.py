598c1bf4d7d7b1f9abcb6be962e28ac6afec6f68
Blaine <rishi1998@gmail.com>
2020-02-19 00:38:37 +0530

    resize init

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -10,6 +10,28 @@ from PyQt5.QtGui import QPainter, QPalette, QColor, QBrush, QPen, QImage
 from PyQt5.QtCore import Qt, QRectF, QPointF
 
 class Circle(QGraphicsItem):
+    handleTopLeft = 1
+    handleTopMiddle = 2
+    handleTopRight = 3
+    handleMiddleLeft = 4
+    handleMiddleRight = 5
+    handleBottomLeft = 6
+    handleBottomMiddle = 7
+    handleBottomRight = 8
+
+    handleSize = +8.0
+    handleSpace = -4.0
+
+    handleCursors = {
+        handleTopLeft: Qt.SizeFDiagCursor,
+        handleTopMiddle: Qt.SizeVerCursor,
+        handleTopRight: Qt.SizeBDiagCursor,
+        handleMiddleLeft: Qt.SizeHorCursor,
+        handleMiddleRight: Qt.SizeHorCursor,
+        handleBottomLeft: Qt.SizeBDiagCursor,
+        handleBottomMiddle: Qt.SizeVerCursor,
+        handleBottomRight: Qt.SizeFDiagCursor,
+    }
     def __init__(self, radius=None, name="", x=0, y=0, parent=None):
         super(Circle, self).__init__(parent)
         self.radius = radius or 50 + random.random() * 300
@@ -22,6 +44,7 @@ class Circle(QGraphicsItem):
         self.setAcceptHoverEvents(True)
         self.setFlag(QGraphicsItem.ItemIsSelectable, True)
         self.setFlag(QGraphicsItem.ItemIsMovable, True)
+        self.updateHandlesPos()
 
     def handleAt(self, point):
         for k, v, in self.handles.items():
@@ -59,6 +82,33 @@ class Circle(QGraphicsItem):
         self.mousePressPos = None
         self.mousePressRect = None
         self.update()
+    
+    def updateHandlesPos(self):
+        s = self.handleSize
+        b = self.boundingRect()
+        self.handles[self.handleTopLeft] = QRectF(b.left(), b.top(), s, s)
+        self.handles[self.handleTopMiddle] = QRectF(b.center().x() - s / 2, b.top(), s, s)
+        self.handles[self.handleTopRight] = QRectF(b.right() - s, b.top(), s, s)
+        self.handles[self.handleMiddleLeft] = QRectF(b.left(), b.center().y() - s / 2, s, s)
+        self.handles[self.handleMiddleRight] = QRectF(b.right() - s, b.center().y() - s / 2, s, s)
+        self.handles[self.handleBottomLeft] = QRectF(b.left(), b.bottom() - s, s, s)
+        self.handles[self.handleBottomMiddle] = QRectF(b.center().x() - s / 2, b.bottom() - s, s, s)
+        self.handles[self.handleBottomRight] = QRectF(b.right() - s, b.bottom() - s, s, s)
+
+    def interactiveResize(self, mousePos):
+        self.prepareGeometryChange()
+        if self.handleSelected in [self.handleTopLeft,
+                                   self.handleTopRight,
+                                   self.handleBottomLeft,
+                                   self.handleBottomRight,
+                                   self.handleTopMiddle,
+                                   self.handleBottomMiddle,
+                                   self.handleMiddleLeft,
+                                   self.handleMiddleRight]:
+            self.radius += (mousePos.y() + mousePos.x() + self.mousePressPos.x() - self.mousePressPos.y())/64
+            self.setPos(self.x(),self.y())
+        self.update()   
+        self.updateHandlesPos()
     
     def boundingRect(self):
         return QRectF(0, 0, self.radius, self.radius)
