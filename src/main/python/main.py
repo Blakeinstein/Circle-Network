4a1fa28f3f6896fa3e97fc10a9983de6a5fbc9ae
Blaine <rishi1998@gmail.com>
2020-02-18 20:24:38 +0530

    added movement boiler plate

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -4,17 +4,182 @@ from sys import exit, platform
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QDialog, QLabel,
                              QPushButton, QGridLayout, QStyleFactory, QHBoxLayout,
-                             QWidget, QComboBox, QGraphicsScene, QGraphicsView)
+                             QWidget, QComboBox, QGraphicsScene, QGraphicsView, QGraphicsItem)
 from PyQt5.QtGui import QPainter, QPalette, QColor, QBrush, QPen, QPixmap, QImage
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRectF
 
-class circle():
-    def __init__(self, radius=None, name=None, x=None, y=None):
-        self.radius = radius if radius else random.random()*500
+class Circle(QGraphicsItem):
+    def __init__(self, radius=None, name="", x=0, y=0, parent=None):
+        super(Circle, self).__init__(parent)
+        self.radius = radius or random.random() * 500
         self.label = name if name else "cirA"
-        self.x = x if x else random.randint(0, 900)
-        self.y = y if y else random.randint(0, 600)
-        # self.center = complex(self.x, self.y)
+        self.setPos(x or random.randint(0, 900), y or random.randint(0, 900))
+        self.handles = {}
+        self.handleSelected = None
+        self.mousePressPos = None
+        self.mousePressRect = None
+        self.setAcceptHoverEvents(True)
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
+        self.setFlag(QGraphicsItem.ItemIsMovable, True)
+
+    def hoverMoveEvent(self, moveEvent):
+        """
+        Executed when the mouse moves over the shape (NOT PRESSED).
+        """
+        if self.isSelected():
+            handle = self.handleAt(moveEvent.pos())
+            cursor = Qt.ArrowCursor if handle is None else self.handleCursors[handle]
+            self.setCursor(cursor)
+        super().hoverMoveEvent(moveEvent)
+
+    def hoverLeaveEvent(self, moveEvent):
+        """
+        Executed when the mouse leaves the shape (NOT PRESSED).
+        """
+        self.setCursor(Qt.ArrowCursor)
+        super().hoverLeaveEvent(moveEvent)
+
+    def mousePressEvent(self, mouseEvent):
+        """
+        Executed when the mouse is pressed on the item.
+        """
+        self.handleSelected = self.handleAt(mouseEvent.pos())
+        if self.handleSelected:
+            self.mousePressPos = mouseEvent.pos()
+            self.mousePressRect = self.boundingRect()
+        super().mousePressEvent(mouseEvent)
+
+    def mouseMoveEvent(self, mouseEvent):
+        """
+        Executed when the mouse is being moved over the item while being pressed.
+        """
+        if self.handleSelected is not None:
+            self.interactiveResize(mouseEvent.pos())
+        else:
+            super().mouseMoveEvent(mouseEvent)
+
+    def mouseReleaseEvent(self, mouseEvent):
+        """
+        Executed when the mouse is released from the item.
+        """
+        super().mouseReleaseEvent(mouseEvent)
+        self.handleSelected = None
+        self.mousePressPos = None
+        self.mousePressRect = None
+        self.update()
+    
+    def interactiveResize(self, mousePos):
+        """
+        Perform shape interactive resize.
+        """
+        offset = self.handleSize + self.handleSpace
+        boundingRect = self.boundingRect()
+        rect = self.rect()
+        diff = QPointF(0, 0)
+
+        self.prepareGeometryChange()
+
+        if self.handleSelected == self.handleTopLeft:
+
+            fromX = self.mousePressRect.left()
+            fromY = self.mousePressRect.top()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setX(toX - fromX)
+            diff.setY(toY - fromY)
+            boundingRect.setLeft(toX)
+            boundingRect.setTop(toY)
+            rect.setLeft(boundingRect.left() + offset)
+            rect.setTop(boundingRect.top() + offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleTopMiddle:
+
+            fromY = self.mousePressRect.top()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setY(toY - fromY)
+            boundingRect.setTop(toY)
+            rect.setTop(boundingRect.top() + offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleTopRight:
+
+            fromX = self.mousePressRect.right()
+            fromY = self.mousePressRect.top()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setX(toX - fromX)
+            diff.setY(toY - fromY)
+            boundingRect.setRight(toX)
+            boundingRect.setTop(toY)
+            rect.setRight(boundingRect.right() - offset)
+            rect.setTop(boundingRect.top() + offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleMiddleLeft:
+
+            fromX = self.mousePressRect.left()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            diff.setX(toX - fromX)
+            boundingRect.setLeft(toX)
+            rect.setLeft(boundingRect.left() + offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleMiddleRight:
+            print("MR")
+            fromX = self.mousePressRect.right()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            diff.setX(toX - fromX)
+            boundingRect.setRight(toX)
+            rect.setRight(boundingRect.right() - offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleBottomLeft:
+
+            fromX = self.mousePressRect.left()
+            fromY = self.mousePressRect.bottom()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setX(toX - fromX)
+            diff.setY(toY - fromY)
+            boundingRect.setLeft(toX)
+            boundingRect.setBottom(toY)
+            rect.setLeft(boundingRect.left() + offset)
+            rect.setBottom(boundingRect.bottom() - offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleBottomMiddle:
+
+            fromY = self.mousePressRect.bottom()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setY(toY - fromY)
+            boundingRect.setBottom(toY)
+            rect.setBottom(boundingRect.bottom() - offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleBottomRight:
+
+            fromX = self.mousePressRect.right()
+            fromY = self.mousePressRect.bottom()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setX(toX - fromX)
+            diff.setY(toY - fromY)
+            boundingRect.setRight(toX)
+            boundingRect.setBottom(toY)
+            rect.setRight(boundingRect.right() - offset)
+            rect.setBottom(boundingRect.bottom() - offset)
+            self.setRect(rect)
+
+        self.updateHandlesPos()
+        
+    def boundingRect(self):
+        return QRectF(0, 0, self.radius, self.radius)
+
+    def paint(self, painter, option, widget):
+        painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+        painter.drawEllipse(0, 0, self.radius, self.radius)
+        painter.drawText(0, 0, self.radius, self.radius, Qt.AlignCenter, self.label)
         
     def __str__(self):
         return f'Circle called {self.label} centered at {self.x}, {self.y}'
@@ -30,13 +195,13 @@ class gui(QDialog):
             QApplication.setPalette(QApplication.style().standardPalette())
         self.setWindowTitle('Circle Networks')
         self.painter = QGraphicsScene(10, 10, 900, 600)
+        self.painter.setBackgroundBrush(QBrush(Qt.white))
         self.createTopLayout()
         self.canvas = QGraphicsView(self.painter)
         mainLayout = QGridLayout()
         mainLayout.addLayout(self.topLayout, 0, 0, 1, 2)
         mainLayout.addWidget(self.canvas, 1, 0, 6, 2)
         self.setLayout(mainLayout)
-        self.circleList = []
 
     def changeCanvasBG(self, style):
         if style == 'white background':
@@ -51,7 +216,7 @@ class gui(QDialog):
         button2 = QPushButton("Generate Report")
         button3 = QPushButton("Save", clicked = self.renderPng)
         styleComboBox = QComboBox()
-        styleComboBox.addItems(['transparent background', 'white background'])
+        styleComboBox.addItems(['white background', 'transparent background'])
         styleComboBox.activated[str].connect(self.changeCanvasBG)
         styleLabel = QLabel("&Style:")
         styleLabel.setBuddy(styleComboBox)
@@ -61,22 +226,17 @@ class gui(QDialog):
         self.topLayout.addWidget(button1)
         self.topLayout.addWidget(button2)
         self.topLayout.addWidget(button3)
-    
-    def drawCircle(self):
-        pen = QPen(Qt.black, 2, Qt.SolidLine)
-        self.painter.clear()
-        for cir in self.circleList:
-            self.painter.addEllipse(cir.x, cir.y, cir.radius, cir.radius, pen=QPen(Qt.black, 2, Qt.SolidLine))
-        self.painter.update()
-        self.canvas.update()
-        self.update()
-    
+        
+    @property
+    def circleList(self):
+        return [item for item in self.painter.items() if isinstance(item, Circle)]
+
     def newCircle(self, cir):
-        self.circleList.append(cir)
-        self.drawCircle()
-    
+        self.painter.addItem(cir)
+        return cir
+
     def addCircle(self):
-        return self.newCircle(circle())
+        return self.newCircle(Circle())
     
     def renderPng(self):
         printed = QImage(300, 200, QImage.Format_ARGB32)
@@ -106,8 +266,8 @@ if __name__ == "__main__":
     app = ApplicationContext()
     test = gui()
     test.show()
-    cir1 = circle(name='cirA')
-    cir2 = circle(name='cirB')
-    test.newCircle(cir = cir1)
-    test.newCircle(cir = cir2)
+    # cir1 = circle(name='cirA')
+    # cir2 = circle(name='cirB')
+    # test.newCircle(cir = cir1)
+    # test.newCircle(cir = cir2)
     exit(app.app.exec_())
